d4749cd65f337143addcbbcbea15d2a74414519e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-23 22:21:07 +0530

    Replaced Radium with Style component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Person.css';
+import styled from 'styled-components';
 // import Radium from 'radium';
 // ES6 style
 // Props is the tecnical ketword for the attribute of an element
@@ -12,8 +13,33 @@ const person = (props) => {
     }
   }
 
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    margin: 16px;
+    @media (min-width: 500px) {
+      width: 450px;
+    }
+  `;
+
   return (
-    <div className='Person' style={style}>
+    // <div className='Person' style={style}>
+    //   <p> I'm a Person!</p>
+    //   <p>Name provided as attribute: {props.name}, age: {props.age} </p>
+    //   <p>Age {Math.floor(Math.random() * 100)}</p>
+    //   <p> {props.children} </p>
+    //   <button onClick={props.deleteButton}>Delete</button>
+    //   <br />
+    //   <input type='text' onChange={props.change} />
+    // </div>
+
+
+    // Style Div
+    <StyledDiv>
       <p> I'm a Person!</p>
       <p>Name provided as attribute: {props.name}, age: {props.age} </p>
       <p>Age {Math.floor(Math.random() * 100)}</p>
@@ -21,7 +47,7 @@ const person = (props) => {
       <button onClick={props.deleteButton}>Delete</button>
       <br />
       <input type='text' onChange={props.change} />
-    </div>
+    </StyledDiv>
   );
 };
 
